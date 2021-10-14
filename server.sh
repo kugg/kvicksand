803911b5247e16be046eeb8573d4def2d95933a9
kugg <kugghjul@gmail.com>
2021-10-14 10:44:46 +0200

    Initial testscript
    AN initial test script that writes a malformed header and some notes on potential changes.

---
+++ server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Variables
+log="./logs/server.pid"
+timeout=300 # 5 minutes
+
+# Server response
+echo HTTP/1.0 200;
+echo Content-Type\: text/plain;
+echo;
+
+# Log the request
+start=$(date +%s);
+log=$log.$start;
+echo $start > $log;
+timeout $timeout cat >> $log;
+date +%s >> $log"
