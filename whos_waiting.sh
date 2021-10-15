c5fc6f74fd90f0b2cde370913dbcaacc79b3e888
kugg <kugghjul@gmail.com>
2021-10-15 16:47:38 +0200

    More stats
    Added ipaddress and verb to the stats.

--- whos_waiting.sh
+++ whos_waiting.sh
@@ -1,7 +1,10 @@
 # A script that calculates kvicksand logfile waittime and prints User-Agent in csv format
-re='^[0-9]+$'
+intre='^[0-9]+$'
+ipre='^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'
 startt=$(head -n 1 $1)
 stopt=$(tail -n 1 $1)
-if [[ $startt =~ $re ]] && [[ $stopt =~ $re ]] ; then
-	echo \"$(grep User-Agent $1| cut -f 2- -d ' ')\", \"$(($stopt - $startt))\"
+ipaddr=$(sed '2q;d' $1)
+verb=$(sed '3q;d' $1)
+if [[ $ipaddr =~ $ipre ]] && [[ $startt =~ $intre ]] && [[ $stopt =~ $intre ]] ; then
+        echo \"$(grep User-Agent $1| cut -f 2- -d ' ')\", \"$(($stopt - $startt))\",\"$ipaddr\",\"$verb\"
 fi
